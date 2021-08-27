a26f29f5dc19d6a9147faf1174dd0ec7c83aae96
Darrell Chaddock <webtastic.dc@gmail.com>
2021-08-26 19:37:20 -0700

    Finished all styling and Javascript - project complete

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ const links = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle("open");
-})
+    links.forEach( link => {
+        link.classList.toggle("fade");
+    });
+});
